d94d32db30e7ee7a470815f46442da072f9cdfd1
Thomas <thomasnagelgaard10@gmail.com>
2026-03-03 10:22:05 +0100

    lots of LTS evolution
    stuck on yaw drift. small steady state r which compounds. steer input doesnt seem to work?

--- MATLAB/VD/LTS.m
+++ MATLAB/VD/LTS.m
@@ -3,6 +3,7 @@ clc; clear; close all;
 %% Chassis
 AR26.Mass = 293; % Total car mass with average driver - kg
 AR26.WheelBase = 1525e-3; % Car wheelbase - m
+AR26.FrontTrackwidt = 1260e-3 % Front Trackwidth
 AR26.CGHeight = 230e-3; % Center of Gravity height - m
 AR26.aLength = AR26.WheelBase*0.45; 
 AR26.bLength = AR26.WheelBase - AR26.aLength; 
@@ -16,7 +17,21 @@ AR26.RearTrackWidth = 1209e-3; % Rear Trackwidth - m
 %% Suspension
 
 %% Steering
-
+AR26.SteeringWheelBreakPointsOutside = [0, 16.7, 30.25, 44.2, 61.4, 78.35, 86];
+AR26.SteeringWheelBreakPointsInside = flip(-AR26.SteeringWheelBreakPointsOutside);
+AR26.SteeringWheelBreakPoints = [AR26.SteeringWheelBreakPointsInside, AR26.SteeringWheelBreakPointsOutside];
+AR26.WheelBreakPointsOutside = [0, 5, 10, 15, 20, 25, 30];
+AR26.WheelBreakPointsInside = flip(AR26.WheelBreakPointsOutside);
+AR26.WheelBreakPoints = [AR26.WheelBreakPointsInside,AR26.WheelBreakPointsOutside];
+figure
+plot(AR26.SteeringWheelBreakPointsOutside, AR26.WheelBreakPointsOutside)
+hold on
+plot(AR26.SteeringWheelBreakPointsInside, AR26.WheelBreakPointsInside)
+AR26.RackWidth = 424.42e-3; % Steering Rack width - m
+AR26.SteeringArmLength = 78.68e-3; % Length from upright tierod point to steering axis - m
+AR26.TieRodLength = 339.4e-3; % Tie Rod length - m
+AR26.RackAxisLength = 79.52e-3; % Long. length from steering rack to steering axis point - m
+AR26.PinionRadius = 20e-3; % Pinion radius - m
 %% Brakes
 AR26.FrntBrkBias = 0.75; % Front brake bias - ratio
 AR26.RearBrkBias = 1 -AR26.FrntBrkBias; % Rear brake bias - ratio
@@ -41,4 +56,9 @@ AR26.RearPressure = 2.1e5;
 %% PowerTrain
 AR26.MaxTorque = 31.6; % Max torque per motor
 
+
+
+%% Initial Conditions
+AR26.Vx0 = 1; % inital x velocity - m/s
+AR26.Omega0 = AR26.Vx0/AR26.LoadedRaidus; % Initial wheelspeed - rad/s
 
