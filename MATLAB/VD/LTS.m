cb4698f7f604b6e4a7f88ffcc8e13af8e33b026a
Thomas <thomasnagelgaard10@gmail.com>
2026-03-11 09:32:32 +0100

    nothing special

--- MATLAB/VD/LTS.m
+++ MATLAB/VD/LTS.m
@@ -12,9 +12,13 @@ AR26.RearTrackWidth = 1209e-3; % Rear Trackwidth - m
 
 
 %% Aerodynamics
+AR26.CL = 4.2; % Coefficient of downforce(lift)
+AR26.CD = 0.8; %Coefficient of drag
+AR26.FrontalArea = 0.8; % Front view area of the car - m^2
 
 
 %% Suspension
+
 
 %% Steering
 AR26.SteeringWheelBreakPointsOutside = [0, 16.7, 30.25, 44.2, 61.4, 78.35, 86];
@@ -23,15 +27,13 @@ AR26.SteeringWheelBreakPoints = [AR26.SteeringWheelBreakPointsInside, AR26.Steer
 AR26.WheelBreakPointsOutside = [0, 5, 10, 15, 20, 25, 30];
 AR26.WheelBreakPointsInside = flip(AR26.WheelBreakPointsOutside);
 AR26.WheelBreakPoints = [AR26.WheelBreakPointsInside,AR26.WheelBreakPointsOutside];
-figure
-plot(AR26.SteeringWheelBreakPointsOutside, AR26.WheelBreakPointsOutside)
-hold on
-plot(AR26.SteeringWheelBreakPointsInside, AR26.WheelBreakPointsInside)
 AR26.RackWidth = 424.42e-3; % Steering Rack width - m
 AR26.SteeringArmLength = 78.68e-3; % Length from upright tierod point to steering axis - m
 AR26.TieRodLength = 339.4e-3; % Tie Rod length - m
 AR26.RackAxisLength = 79.52e-3; % Long. length from steering rack to steering axis point - m
 AR26.PinionRadius = 20e-3; % Pinion radius - m
+
+
 %% Brakes
 AR26.FrntBrkBias = 0.75; % Front brake bias - ratio
 AR26.RearBrkBias = 1 -AR26.FrntBrkBias; % Rear brake bias - ratio
@@ -45,9 +47,9 @@ AR26.DiscRadius = 0.177; % Brake disc radius - meters
 AR26.ScaleFctrs = ones(27,1);
 AR26.Fext = [(293*9.81)/4 (293*9.81)/4 (293*9.81)/4 (293*9.81)/4];
 AR26.UnloadedRadius = 0.203; % Unloaded tyre radius - m
-AR26.LoadedRadius = 0.2; % Loaded tyre radius - m
-AR26.FrontPressure = 1e5;
-AR26.RearPressure = 1.1e5;
+AR26.LoadedRadius = 0.197; % Loaded tyre radius - m
+AR26.FrontPressure = 2e5;
+AR26.RearPressure = 2.1e5;
 AR26.TireWidth = 7.5*25.4e-3;
 AR26.RimRadius = 5*25.4e-3;
 
@@ -62,5 +64,6 @@ AR26.MaxTorque = 31.6; % Max torque per motor
 
 %% Initial Conditions
 AR26.Vx0 = 0; % inital x velocity - m/s
+AR26.Vy0 = 0; % initial y velocity - m/s
 AR26.Omega0 = AR26.Vx0/AR26.LoadedRadius; % Initial wheelspeed - rad/s
 
