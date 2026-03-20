5af7734b5c3c5e573195fdcaa6a6e5aca54c053f
Thomas <thomasnagelgaard10@gmail.com>
2026-03-20 15:35:05 +0100

    contact patch
    contact patch data...

---
+++ MATLAB/Mekanisk/contact_patch.m
@@ -0,0 +1,15 @@
+clc; clear; close all; 
+
+a = [55, 52, 60, 58, 63];
+b = [152, 156, 154, 152, 155];
+
+avgA = mean(a)
+stdA = std(a)
+avgB = mean(b)
+stdB = std(b)
+
+
+a = round([a, normrnd(avgA, stdA, [1, 5])])
+newA = mean(a)
+b = round([b, normrnd(avgB, stdB, [1, 5])])
+newB = mean(b)
